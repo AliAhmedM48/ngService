71a002ced09bd93e5706233379a1bac669426f33
Ali Ahmed <MrAliAhmedM@gmail.com>
2024-02-08 10:23:02 +0200

    create main layout

--- projects/app2/src/app/app.routes.ts
+++ projects/app2/src/app/app.routes.ts
@@ -3,12 +3,18 @@ import { HomeComponent } from './components/home/home.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { UsersComponent } from './components/users/users.component';
 import { UserDetailsComponent } from './components/user-details/user-details.component';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
 
 export const routes: Routes = [
-    { path: 'home', component: HomeComponent, title: 'Home' },
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'users', component: UsersComponent, title: 'Users' },
-    { path: 'users/:id', component: UserDetailsComponent, title: 'User details' },
+    {
+        path: '', component: MainLayoutComponent, children: [
+            { path: 'home', component: HomeComponent, title: 'Home' },
+            { path: '', redirectTo: '/home', pathMatch: 'full' },
+            { path: 'users', component: UsersComponent, title: 'Users' },
+            { path: 'users/:id', component: UserDetailsComponent, title: 'User details' },
+        ]
+    },
+
     { path: 'not-found', component: NotFoundComponent, title: 'Not found' },
     { path: '**', redirectTo: 'not-found' },
 
